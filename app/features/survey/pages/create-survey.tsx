059b0acbae4d0a033531d7e6d9912767483b6784
IsuminI <suminwow1@o.cnu.ac.kr>
2025-10-26 15:50:07 +0900

    add react, navigation, routing, wallet connection

---
+++ app/features/survey/pages/create-survey.tsx
@@ -0,0 +1,3 @@
+export default function CreateSuvey() {
+  return <div>Create Survey</div>;
+}
--- app/root.tsx
+++ app/root.tsx
@@ -9,19 +9,23 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import React from "react";
+import Navigation from "./components/ui/navigation";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+import { createModal } from "@rabby-wallet/rabbykit";
+import { createConfig, http } from "@wagmi/core";
+import { hardhat } from "@wagmi/core/chains";
+
+export const config = createConfig({
+  chains: [hardhat],
+  transports: {
+    [hardhat.id]: http(),
   },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+});
+
+export const rabbykit = createModal({
+  wagmi: config,
+});
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -42,7 +46,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div className="py-20 px-5">
+      <Navigation />
+      <Outlet />
+    </div>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
