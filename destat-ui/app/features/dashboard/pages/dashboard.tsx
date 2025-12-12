fa0fe46bb6a13eaca4bbf52d04a43eb1f71a4bd9
IsuminI <suminwow1@o.cnu.ac.kr>
2025-12-12 20:49:25 +0900

    Add Dashboard Pages and Components

--- destat-ui/app/features/dashboard/pages/dashboard.tsx
+++ destat-ui/app/features/dashboard/pages/dashboard.tsx
@@ -1,5 +1,3 @@
-import { supabase } from "~/postgres/supaclient";
-
 // - navigation
 //     - Dashboard
 //     - Survey
@@ -11,12 +9,77 @@ import { supabase } from "~/postgres/supaclient";
 //        - My surveys
 //        - My responses
 
-// before rendering Home react component
-export async function loader() {
-  const { data } = await supabase().from("destat-test").select("*");
-  console.log(data);
-}
+import TrendCard from "../components/trend-card";
+import { TrendChart } from "../components/trend-chard";
 
-export default function Home() {
-  return <div>Hello destat world</div>;
+// [Number]
+// Visitors
+// Live Surveys
+// Archived Surveys
+
+// [Graph]
+// time x Live Surveys
+// time x Archived Surveys
+
+const data = [
+  { date: "2025-10-01", data: 186 },
+  { date: "2025-10-02", data: 190 },
+  { date: "2025-10-03", data: 188 },
+  { date: "2025-10-04", data: 192 },
+  { date: "2025-10-05", data: 195 },
+  { date: "2025-10-06", data: 193 },
+  { date: "2025-10-07", data: 198 },
+  { date: "2025-10-08", data: 200 },
+  { date: "2025-10-09", data: 202 },
+  { date: "2025-10-10", data: 199 },
+  { date: "2025-10-11", data: 205 },
+  { date: "2025-10-12", data: 207 },
+  { date: "2025-10-13", data: 210 },
+  { date: "2025-10-14", data: 208 },
+];
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="grid grid-cols-3 gap-5 mt-10 w-full">
+        <TrendCard
+          title={"Total Visitors"}
+          value={"123,123,123"}
+          trendValue={"200%"}
+          trendMessage={"Trending up"}
+          periodMessage={"last 6 months "}
+        />
+        <TrendCard
+          title={"Live Visitors"}
+          value={"123"}
+          trendValue={"200%"}
+          trendMessage={"Trending up"}
+          periodMessage={"last 6 months "}
+        />
+        <TrendCard
+          title={"Archived Visitors"}
+          value={"123,123"}
+          trendValue={"200%"}
+          trendMessage={"Trending up"}
+          periodMessage={"last 6 months "}
+        />
+      </div>
+      <div className="grid grid-cols-2 mt-5 gap-5 w-full">
+        <TrendChart
+          title={"Live Surveys"}
+          description={"Daily live survey count"}
+          trendMessage={""}
+          periodMessage={""}
+          chartData={data}
+        />
+        <TrendChart
+          title={"Archived Surveys"}
+          description={"Daily archived survey count"}
+          trendMessage={""}
+          periodMessage={""}
+          chartData={data}
+        />
+      </div>
+    </div>
+  );
 }
